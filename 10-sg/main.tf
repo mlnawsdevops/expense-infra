9de1421f76a2864e0cb7cf3264c77109f2bd53c1
mlnawsdevops <dev@123.email>
2026-02-11 19:34:31 +0530

    jenkins

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -159,3 +159,21 @@ resource "aws_security_group_rule" "bastion_public" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = module.bastion_sg.id
 }
+
+resource "aws_security_group_rule" "bastion_mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.mysql_sg.id
+  security_group_id        = module.bastion_sg.id 
+}
+
+resource "aws_security_group_rule" "node_mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.mysql_sg.id
+  security_group_id        = module.node_sg.id 
+}
